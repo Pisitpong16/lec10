53cef402efb926d879a7873373f49eb6d173e434
Pisitpong16 <kreangkrailipikonp@gmail.com>
2022-10-27 15:45:58 +0700

    inclass

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,26 @@
+class MainController < ApplicationController
+  def login
+  end
+  
+  def create
+    u = User.where(login: params[:login]).first
+    if u && u.authenticate(params[:password])
+
+      redirect_to main_user_Item_path
+
+      session[:logged_in] = true
+      session[:login_user_id] = u.id
+    else 
+      redirect_to main_login_path, notice: 'wrong username or password'
+    end
+  end
+
+  def user_Item
+    @user_id = session[:login_user_id]
+    @user = User.where( id:@user_id ).first
+    
+  end
+
+
+ 
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'main/login'
+  post 'main/create'
+  get 'main/user_Item'
+  resources :inventories
+  resources :items
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
